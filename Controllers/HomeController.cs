e252aa3fed4071649fd99ec0515b4b4ea63eea26
Jonathan Kwon <jonathankwon@yahoo.com>
2022-06-09 16:00:58 -0700

    init commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers;
+
+public class HomeController : Controller
+{
+    [HttpGet("")]
+    public ViewResult Index() 
+    {
+        return View();
+    }
+
+    [HttpPost("method")]
+    public IActionResult Method(Survey survey)
+    {
+        return RedirectToAction("Result", survey);
+    }
+
+    [HttpGet("result")]
+    public ViewResult Result (Survey survey)
+    {
+        
+        return View("Result", survey);
+    }
+
+
+}
+
